876fed5ef5250a424cdd24cd820510100fe4f4f6
Andrey Loginov <andrey12q112@gmail.com>
2024-02-26 23:43:46 +0300

    feat: add game database

---
+++ src/db/gameDb.ts
@@ -0,0 +1,31 @@
+import { type IGame } from 'models/Game.interface';
+
+export class GameDatabase {
+  private readonly games: IGame[];
+
+  constructor() {
+    this.games = [];
+  }
+
+  getAllGames(): IGame[] {
+    return this.games;
+  }
+
+  addGame(game: IGame): void {
+    this.games.push(game);
+  }
+
+  deleteGame(gameId: number): void {
+    const index = this.games.findIndex((game) => game.gameId === gameId);
+    if (index !== -1) {
+      this.games.splice(index, 1);
+    }
+  }
+
+  updateGame(gameId: number, updatedGame: Partial<IGame>): void {
+    const game = this.games.find((game) => game.gameId === gameId);
+    if (game) {
+      Object.assign(game, updatedGame);
+    }
+  }
+}
